1b03b00622f4273683760627627bf884ec671068
Pavan S Sabnis <pvnsbns@gmail.com>
2018-10-11 15:23:44 -0700

    Add UI elements, connect to code in ViewController

--- DiceRoller/DiceRoller/ViewController.swift
+++ DiceRoller/DiceRoller/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var diceImageView: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageView = UIImageView(image: UIImage(named: "dice2"))
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,15 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func rollButtonTapped(_ sender: Any) {
-        print("roll button taped")
-        var diceFaces = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
-        var diceNumber:Int = Int(arc4random_uniform(6))
-        print(diceNumber)
-        var diceName = diceFaces[diceNumber]
-        print(diceName)
-        diceImageView = UIImageView(image: UIImage(named: diceName))
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
     }
+    
     
     
 
