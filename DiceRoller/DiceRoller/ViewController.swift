8c72b1729ae26a51a16de85873b798a6a419ade0
Pavan S Sabnis <pvnsbns@gmail.com>
2018-10-12 22:59:06 -0700

    add randmozation functionality for dice image, add dice image changing functionality when roll button is pressed

--- DiceRoller/DiceRoller/ViewController.swift
+++ DiceRoller/DiceRoller/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
     
     
     override func viewDidLoad() {
@@ -26,7 +28,16 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rollButtonPressed(_ sender: Any) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
         
+        print("\(randomDiceIndex1), \(randomDiceIndex2)")
+        
+        var diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
     
